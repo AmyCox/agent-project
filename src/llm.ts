722c5862cac5c07da0588d1171d34d784ceb6370
AmyCox <amy_cox@me.com>
2025-06-14 11:57:27 +0100

    first commit

--- src/llm.ts
+++ src/llm.ts
@@ -1,13 +1,24 @@
+import type { AIMessage } from '../types';
 import { openai } from "./ai";
+import { zodFunction } from 'openai/helpers/zod';
+import {z} from 'zod';
 
-export const runLLM = async ({userMessage}: { userMessage: string }) => {
+
+
+export const runLLM = async ({ model = 'gpt-4o-mini', messages, temperature = 0.1, tools }: { model?:string, messages: AIMessage[], temperature?: number, tools?: {name: string; parameters: z.AnyZodObject}[] }) => {
+
+
+  const formattedTools = tools?.map((tool) => zodFunction(tool));
+
+
   const response = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
     temperature: 0.1,
-    messages: [
-      {role: 'user', content: userMessage }
-    ]
+    messages,
+    tools:formattedTools,
+    tool_choice: 'auto', 
+    parallel_tool_calls: false
   })
-  return response.choices[0].message.content
+  return response.choices[0].message
 }
 
